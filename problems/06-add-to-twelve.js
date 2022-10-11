db8c3dcdd0c22d725ad93536934131dc8ef960d1
Brian Washington <bw49078@gmail.com>
2022-10-11 12:14:19 -0500

    finished

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -15,7 +15,13 @@ addToTwelve([1]); // false
 
 function addToTwelve(arr) {
   // Your code here
+  if (arr.length < 2) return false;
+  if (arr[0] + arr[1] === 12) return true;
+
+  return addToTwelve(arr.slice(1));
 }
+
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
